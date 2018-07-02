59c880c6b439ab6839458fe2907a032987335ebc
Adam Watt <awatt@cloudflare.com>
2018-07-02 12:05:46 -0700

    update redirect endpoint

--- main.go
+++ main.go
@@ -34,11 +34,11 @@ func IndexEndPoint(w http.ResponseWriter, r *http.Request) {
 
 // ShortenURLEndPoint Add description here.
 func ShortenURLEndPoint(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	urlToBeShortened := r.Form["url"][0]
+	params := mux.Vars(r)
+	urlToBeShortened := params["url"]
 	slug, _ := shortid.Generate()
 	date := time.Now()
-	newURL := &url{
+	newURL := url{
 		LongURL:  urlToBeShortened,
 		ShortURL: slug,
 		Date:     date,
@@ -50,7 +50,7 @@ func ShortenURLEndPoint(w http.ResponseWriter, r *http.Request) {
 	})
 	defer db.Close()
 
-	err := db.Insert(newURL)
+	err := db.Insert(&newURL)
 	if err != nil {
 		panic(err)
 	}
@@ -64,7 +64,34 @@ func ShortenURLEndPoint(w http.ResponseWriter, r *http.Request) {
 
 // RedirectEndPoint Add description here.
 func RedirectEndPoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	params := mux.Vars(r)
+	slug := params["slug"]
+	db := pg.Connect(&pg.Options{
+		Addr: "db:5432",
+		User: "postgres",
+	})
+	defer db.Close()
+	urlToBeLoaded, err := getlongURL(db, slug)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("URL Not Found. Error: " + err.Error() + "\n"))
+		return
+	}
+	fmt.Printf("urlToBeLoaded = %#v \n", urlToBeLoaded)
+	LongURL := urlToBeLoaded.LongURL
+	fmt.Printf("LongURL  = %#v \n", LongURL)
+	http.Redirect(w, r, LongURL, 302)
+}
+
+// GetlongURL Add Description here
+func getlongURL(db *pg.DB, slug string) (*url, error) {
+	var url url
+	err := db.Model(&url).
+		Where("url.short_url = ?", slug).
+		Select()
+	fmt.Printf("&url  = %#v \n", &url)
+	fmt.Printf("err  = %#v \n", err)
+	return &url, err
 }
 
 // StatsEndPoint Add description here.
@@ -73,7 +100,6 @@ func StatsEndPoint(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fmt.Println("Welcome To Shortly !")
 	db := pg.Connect(&pg.Options{
 		Addr: "db:5432",
 		User: "postgres",
@@ -86,8 +112,8 @@ func main() {
 	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", IndexEndPoint).Methods("GET")
-	r.HandleFunc("/api/shorten", ShortenURLEndPoint).Methods("POST")
-	r.HandleFunc("/slug", RedirectEndPoint).Methods("GET")
+	r.HandleFunc("/api/shorten/{url}", ShortenURLEndPoint).Methods("POST")
+	r.HandleFunc("/{slug}", RedirectEndPoint).Methods("GET")
 	r.HandleFunc("/stats/:slug/:statsParam", StatsEndPoint).Methods("GET")
 	if err := http.ListenAndServe(":6060", r); err != nil {
 		log.Fatal(err)
